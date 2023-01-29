3a0b68ba4458f060e6c202fdf4a489b049e887b6
Muhammad-Abid11 <abidkhan11845@gmail.com>
2023-01-30 02:50:11 +0500

    Use of inline Style and Image tag

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 42, color: "red" }}>Hello World</Text>
+
+      <Image
+        style={
+          { width: 200, height: 200 }
+        } //without size picture will not preview
+        source={
+          { uri: "https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fA%3D%3D&w=1000&q=80" }
+        }
+      />
+
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 100,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
